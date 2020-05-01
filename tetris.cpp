92bc054fbd038cc1f4b04ce8c50cfc42f06320b2
Jesse Chan <jesse.tk.chan@gmail.com>
2020-04-30 23:04:46 -0700

    initial set up

---
+++ tetris.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//tutorial: https://www.youtube.com/watch?v=8OK8_tHeCIA
+//code help: https://github.com/OneLoneCoder/videos/blob/master/OneLoneCoder_Tetris.cpp
+//http://javilop.com/gamedev/tetris-tutorial-in-c-platform-independent-focused-in-game-logic-for-beginners/
+
+int main()
+{
+    
+}
